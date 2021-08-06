0f30e6c3801fbc53acafd9545e6bed8b5e305409
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2021-08-07 03:21:27 +0600

    Tanzib instruction - project structure

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const moongoose = require('mongoose');
 const config = require('config');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 moongoose.connect(config.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 db = moongoose.connection;
