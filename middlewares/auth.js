2cd3daaad868f566295d5bb9a28cfef852e4bf58
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2021-08-14 03:01:59 +0600

    Custom auth middleware & JWT - initialization - Next: implement to a protective route

---
+++ middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = (req,res,next) => {
+    // Get the token from the header
+    const token = req.header('a-auth-token');
+    // Check if no token
+    if(!token){
+        res.status(401).json({msg: 'No token authorization, denied!'})
+    }
+    // Verify token
+    try{
+        const dicoded = jwt.verify(token, config.get('jwtSecret'));
+        req.user = decoded.user;
+        next();
+    }catch(err){
+        res.status(401).json({msg: 'Failed token validation, denied'})
+    }
+};
